b9ee136de7590abab13351ab6b4c271683a01a3e
Oscar Calle <gameplaysoscar@gmail.com>
2022-02-19 11:29:07 -0500

    Predecir funciones

---
+++ CALLE_OSCAR_JSPredicción.js
@@ -0,0 +1,18 @@
+// Predicción 1: El año en que nacio: Naci en 1980
+function myBirthYearFunc() {
+	console.log('Nací en ' + 1980);
+}
+
+// Predicción 2: El año en que nacio dependiendo de la variable que se envie: Naci en 1980
+function myBirthYearFunc(EntradaAñoNacimiento) {
+	console.log('Nací en ' + EntradaAñoNacimiento);
+}
+
+// Predicción 3: La suma de los dos parametros num1 + num2 que es igual a 30
+function add(num1, num2) {
+	console.log('¡Sumando números!');
+	console.log('num1 is: ' + num1);
+	console.log('num2 is: ' + num2);
+	var sum = num1 + num2;
+	console.log(sum);
+}
